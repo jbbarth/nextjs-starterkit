a5b1c0cbde18ad6c1cf036eabde72a9180f1e8e5
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2018-08-11 15:53:14 +0200

    Integrate tailwindcss framework

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,24 @@
 import Link from 'next/link'
 
-const linkStyle = {
-  marginRight: 15
-}
-
 const Header = () => (
   <div>
-    <h1>FOOBAR.tools.botify.com</h1>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
+    <div className="mt-2 p-2 text-xl font-semibold">
+      <span className="text-orange-darker">FOOBAR</span>.example.com
+    </div>
+    <nav className="border-b border-t border-grey-light p-3 mb-2">
+      <ul className="list-reset flex font-bold">
+        <li className="mr-6">
+          <Link prefetch href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li className="mr-6">
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 )
 
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+import Document, { Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage()
+    return { html, head, errorHtml, chunks }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/static/css/bundle.css" />
+        </Head>
+        <body>
+          <div className="container mx-auto px-2">
+            <Main />
+          </div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
