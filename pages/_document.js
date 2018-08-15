ed24b3aa816977d0f905079dbaec48ec66738495
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2018-08-15 15:12:01 +0200

    Use Document.getInitialProps from nextjs docs

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ import React from "react"
 import Document, { Head, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    return { html, head, errorHtml, chunks }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
